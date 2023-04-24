57510514f96e5f36a8722ee46d8f362567b420ee
Zach Parkhurst <zparkhurst11@gmail.com>
2023-04-24 13:52:19 -0400

    I fixed the hamburger menu it now works

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function load(){
+  var link = document.getElementById("menuButton");
+  link.addEventListener('click', myFunction);  
+}
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
