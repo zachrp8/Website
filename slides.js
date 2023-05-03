69a303f7246116ace10042811cec92a97e8e02f1
Zach Parkhurst <zparkhurst11@gmail.com>
2023-05-03 14:11:27 -0400

    slideshow works need to clean it up and add img

---
+++ slides.js
@@ -0,0 +1,33 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("box"); 
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length-1) {
+        slideIndex = 0;
+        console.log(slides.length)
+    }
+    if (n < 0) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
