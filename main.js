b4f79275e2819bbd6f2b0db09d8c8ac2e9304651
Zach Parkhurst <zparkhurst11@gmail.com>
2023-05-04 16:38:09 -0400

    created a button to change light and dark mode

---
+++ main.js
@@ -0,0 +1,13 @@
+function getStyles() {
+    let x = document.getElementById("mode");
+    let y = document.getElementsByTagName("link")[0];
+    let cssStyle= x.value;
+    y.setAttribute("href", cssStyle);
+}
+
+
+//need to do data validation for math form
+
+
+
+//horizontal slider
