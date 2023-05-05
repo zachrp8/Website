063a94ed3c00e4d3bf133123d7d582bfe7f0bdc5
Zach Parkhurst <zparkhurst11@gmail.com>
2023-05-05 14:16:57 -0400

    added rainbow button

--- main.js
+++ main.js
@@ -4,10 +4,22 @@ function getStyles() {
     let cssStyle= x.value;
     y.setAttribute("href", cssStyle);
 }
+function randomcolor() {
+    const color = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + color;
+    const color2 = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.color = "#" + color2;
+    const color21 = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementsByTagName("header")[0].style.backgroundColor= "#" + color21;
+    const color3 = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementsByTagName("footer")[0].style.backgroundColor= "#" + color3;
+    const color4 = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementsByTagName("footer")[0].style.color= "#" + color4;
+}
 
-
-//need to do data validation for math form
-
-
-
-//horizontal slider
+function validateForm() {
+    let inputs = document.getElementsByClassName("input");
+    for (let i =0; i < inputs.length; i++) {
+        inputs[i].
+    }
+}
